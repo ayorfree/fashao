9a0d36315ffdd35aaba5cd8b6a9391cc04b631c6
zhangjiyun <ayorfree@gmail.com>
2018-04-22 14:04:31 +0800

    add other_base_mag

---
+++ src/fashao/servlet/UserServlet.java
@@ -0,0 +1,41 @@
+package fashao.servlet;
+
+import fashao.util.Page;
+import fashao.bean.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class UserServlet extends BaseBackServlet {
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+
+        return null;
+    }
+
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    public String edit(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        List<User> us = userDAO.list(page.getStart(),page.getCount());
+        int total = userDAO.getTotal();
+        page.setTotal(total);
+
+        request.setAttribute("us", us);
+        request.setAttribute("page", page);
+
+        return "admin/listUser.jsp";
+    }
+}
